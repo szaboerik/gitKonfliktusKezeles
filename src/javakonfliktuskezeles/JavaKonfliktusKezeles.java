347bd7387d396efc4a8966a25c9121db01581f75
szaboerik <78268337+szaboerik@users.noreply.github.com>
2022-03-17 17:38:50 +0100

    2. név inicializálása

--- src/javakonfliktuskezeles/JavaKonfliktusKezeles.java
+++ src/javakonfliktuskezeles/JavaKonfliktusKezeles.java
@@ -2,7 +2,7 @@ package javakonfliktuskezeles;
 public class JavaKonfliktusKezeles {
     public static void main(String[] args) {
         String nev1;
-        String nev2;
+        String nev2 = "X.Y.";
     }
     
 }
